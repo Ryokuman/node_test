5ca4943020ebea910926e4428376fe0869609460
Ryokuman <ryokuman@naver.com>
2023-03-29 15:56:30 +0900

    feature ( components ) : Button

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useInterval } from "../utils/useInterval";
+import api from "../utils/api";
+
+function Button(props) {
+  const [isClicked, setIsClicked] = useState(false);
+  const buttonStr = {
+    backgroundColor: isClicked ? "#333538" : "#57A0E5",
+    width: "100px",
+    height: "50px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: isClicked ? "#57A0E5" : "#333538",
+    cursor: "pointer",
+    borderRadius: "6px",
+    margin: "10px",
+  };
+
+  const onClick = () => setIsClicked(!isClicked);
+  useInterval(() => {
+    if (isClicked) api.post("", { core: props.core });
+  }, 1000);
+
+  return (
+    <div onClick={onClick} style={buttonStr}>
+      CORE NO.{props.core}
+    </div>
+  );
+}
+
+export { Button };
