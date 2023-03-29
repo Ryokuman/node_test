5b2e3fc67c011ac83a70e2e222f176634aa99648
Ryokuman <ryokuman@naver.com>
2023-03-29 16:35:42 +0900

    feature ( frontend/App ) : finish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import Core from "./pages/Core";
 import { Button } from "./components/Button";
 
 function App() {
+  const [cores, setCore] = useState(0);
+  const onAddClick = () => setCore(cores + 1);
+  const onDelClick = () => setCore(cores - 1 <= 0 ? 0 : cores - 1);
+
   return (
     <div>
-      <Button core={1} />
-      <Button core={2} />
-      <Button core={3} />
-      <Button core={4} />
+      <div>
+        {Array.from({ length: cores }, (_, i) => (
+          <Core core={i + 1} key={i} />
+        ))}
+      </div>
+      <div style={{ display: "flex" }}>
+        <Button onClick={onAddClick} value="addTask" />
+        <Button onClick={onDelClick} value="delTask" />
+      </div>
     </div>
   );
 }
